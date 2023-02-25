f70285e4e72f1cdf1559e951c760aa2256af73e1
voicedown <84558730+voicedown@users.noreply.github.com>
2023-02-25 21:42:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ http.listen(port, ()=>{
 });
 
 app.get("/",(req,res)=>{
-    res.send("Wohoo.. Our server is live now");
+    res.send("Wohoo.. Our server is live nowggggg");
 });
 /*
 app.post("/broadcast",(req, res)=>{
