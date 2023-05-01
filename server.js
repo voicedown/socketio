58f1706a7ebb36997d0858b1ef0a2946b6ff76e6
voicedown <84558730+voicedown@users.noreply.github.com>
2023-05-01 12:35:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,17 +36,27 @@ app.post("/broadcast",(req, res)=>{
 socket.on("connect", (io)=>{
     console.log("New User Connected.  ID : " + io.id);
     
-var io         = require('socket.io')();
-var middleware = require('socketio-wildcard')();
-
-io.use(middleware);
-
-io.on('connection', function(socket) {
-  socket.on('*', function(){ /* … */ });
-	console.log("New User Connected.  ID : " + '*');
-});
-
-io.listen(3000);
+var socket = io.connect();
+var globalEvent = "*";
+socket.$emit = function (name) {
+    if(!this.$events) return false;
+    for(var i=0;i<2;++i){
+        if(i==0 && name==globalEvent) continue;
+        var args = Array.prototype.slice.call(arguments, 1-i);
+        var handler = this.$events[i==0?name:globalEvent];
+        if(!handler) handler = [];
+        if ('function' == typeof handler) handler.apply(this, args);
+        else if (io.util.isArray(handler)) {
+            var listeners = handler.slice();
+            for (var i=0, l=listeners.length; i<l; i++)
+                listeners[i].apply(this, args);
+        } else return false;
+    }
+    return true;
+};
+socket.on(globalEvent,function(event){
+    var args = Array.prototype.slice.call(arguments, 1);
+    console.log("Global Event = "+event+"; Arguments = "+JSON.stringify(args));
 });
 
 /* NOTE
