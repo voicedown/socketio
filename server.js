2ca82f865526d03f0580e6665555f662ec9ad592
voicedown <84558730+voicedown@users.noreply.github.com>
2023-05-01 13:55:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-const http = require('http');
-const server = http.createServer();
-const io = require('socket.io')(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
   console.log('A client has connected');
-
-  // Listen for 'dynamic-event' events
-  socket.on('dynamic-event', (data) => {
-    console.log(`Received dynamic-event with data: ${data}`);
-
-    // Get the dynamic event name from the data object
-    const eventName = data.eventName;
-    
-    // Remove the eventName property from the data object
-    delete data.eventName;
-
-    // Emit the event with the dynamic name and data
-    socket.emit(eventName, data);
-  });
 
   socket.on('disconnect', () => {
     console.log('A client has disconnected');
   });
 });
 
-server.listen(3000, () => {
+http.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
