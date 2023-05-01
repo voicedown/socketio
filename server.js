a2017bdc1f33c5d18bb1dcc0eda707276f438b29
voicedown <84558730+voicedown@users.noreply.github.com>
2023-05-01 11:11:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ socket.on("connect", (io)=>{
     
 	
     /*Start Listning to the client request*/
-    io.on((event,data)=>{
-	    console.log("Name : " + event);
+    io.on("some_event",(data)=>{
+	    console.log("Name : " + io.event);
         /* 
            New_Message is the event name on which we'll emit & Listen to data to & from our app
            you can rename 'New_Message' with your desired event name.
